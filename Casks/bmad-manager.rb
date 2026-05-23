07910a916521094ee6b34de00401af108ee94b63
Lauri Palokangas <lauri.palokangas@gmail.com>
2026-05-23 13:28:08 +0300

    Initial tap with bmad-manager cask

---
+++ Casks/bmad-manager.rb
@@ -0,0 +1,22 @@
+cask "bmad-manager" do
+  version "0.0.0"
+  sha256 :no_check
+
+  url "https://github.com/lpalokan/bmad-manager/releases/download/v#{version}/bmad-manager.dmg"
+  name "bmad-manager"
+  desc "Creates new project folders pre-configured with the BMad method"
+  homepage "https://github.com/lpalokan/bmad-manager"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "bmad-manager.app"
+
+  quarantine false
+
+  zap trash: [
+    "~/Library/Application Support/bmad-manager",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# lpalokan's Homebrew tap
+
+Personal Homebrew tap for [lpalokan](https://github.com/lpalokan) projects.
+
+## Install
+
+brew install --cask lpalokan/tap/bmad-manager
+
+See [bmad-manager](https://github.com/lpalokan/bmad-manager) for what the app does.
+
